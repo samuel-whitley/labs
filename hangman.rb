3db24cda536a31da81cd3b8c9b1cc1d9c404cb33
samuel whitley <samuelwhitley@samuels-MacBook-Pro.local>
2015-01-07 19:54:18 -0500

    hangman kinda working

--- hangman.rb
+++ hangman.rb
@@ -2,18 +2,18 @@
 
 require 'pry'
 require 'set'
-#words = ['test']
-words = ['angus',
-         'onomatopeia',
-         'mississippi',
-         'cookies',
-         'terminal',
-         'illness',
-         'communist',
-         'dictator',
-         'capitalist',
-         'marxist',
-         'darwinism']
+words = ['test']
+#words = ['angus',
+         #'onomatopeia',
+         #'mississippi',
+         #'cookies',
+         #'terminal',
+         #'illness',
+         #'communist',
+         #'dictator',
+         #'capitalist',
+         #'marxist',
+         #'darwinism']
 
 def finished? (turns, guesses, answer)
   turns.zero? || answer.chars.all? { |l| guesses.include?(l) }
@@ -23,7 +23,21 @@ def greeting
   puts "Welcome to hang man"
 end
 
-def game_over(turn_count)
+
+binding.pry
+def Display(guessed,answer)
+
+print answer
+end
+#binding.pry
+def prompt_player()
+  #binding.pry
+  #if guess.length != 1
+  #return guess = get.chomp
+  guess = $stdin.gets.chomp
+end
+
+def game_over(turn_count,guessed,answer)
   if turn_count == 0
     puts "YOU LOSE"
   else
@@ -31,16 +45,15 @@ def game_over(turn_count)
   end
 end
 
-def prompt_player
-  #binding.pry
-  return guess = $stdin.gets.chomp
-end
+
+
 
  def hangman(words)
   turn_count = ARGV.empty? ? 6 : ARGV[0].to_i
   guessed = Set.new
   answer = words.sample(1)[0] # words[rand(words.length)]
   greeting
+  #Display(guessed,answer)
   #binding.pry
   until finished?(turn_count, guessed, answer)
     guess = prompt_player
@@ -49,8 +62,8 @@ end
       turn_count -= 1
     end
    end
-  game_over(turn_count)
-
+  game_over(turn_count,guessed,answer)
+  Display(guessed,answer)
  end
 
  hangman(words)
